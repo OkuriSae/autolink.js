5670b537d6775fb052fc7dd96b2d6167b10028fc
atomer <mmroad@hotmail.co.jp>
2012-08-16 10:58:56 +0900

    add target attribute

--- test/test.js
+++ test/test.js
@@ -2,11 +2,13 @@
 var assert = require('assert');
 var al = require('../lib/autolink.js');
 
-assert.equal(al.autolink("http://google.com/"), "<a href='http://google.com/'>http://google.com/</a>");
-assert.equal(al.autolink("http://google.com foo"), "<a href='http://google.com'>http://google.com</a> foo");
-assert.equal(al.autolink("http://google.com/#yay"), "<a href='http://google.com/#yay'>http://google.com/#yay</a>");
-assert.equal(al.autolink("http://google.com/ yay!"), "<a href='http://google.com/'>http://google.com/</a> yay!");
+assert.equal(al.autolink("http://google.com/"), '<a href="http://google.com/">http://google.com/</a>');
+assert.equal(al.autolink("http://google.com foo"), '<a href="http://google.com">http://google.com</a> foo');
+assert.equal(al.autolink("http://google.com/#yay"), '<a href="http://google.com/#yay">http://google.com/#yay</a>');
+assert.equal(al.autolink("http://google.com/ yay!"), '<a href="http://google.com/">http://google.com/</a> yay!');
+assert.equal(al.autolink("http://google.com/ yay!", "_blank"), '<a href="http://google.com/" target="_blank">http://google.com/</a> yay!');
 assert.equal(al.autolink("foo > @yappo heh."), 'foo &gt; @yappo heh.');
-assert.equal(al.autolinkTwitter("http://mixi.jp @yappo #foo hoge"), "<a href='http://mixi.jp'>http://mixi.jp</a> <a href='http://twitter.com/yappo'>@yappo</a> <a href='https://twitter.com/#!/search/?q=%23foo'>#foo</a> hoge");
+assert.equal(al.autolinkTwitter("http://mixi.jp @yappo #foo hoge"), '<a href="http://mixi.jp">http://mixi.jp</a> <a href="http://twitter.com/yappo">@yappo</a> <a href="https://twitter.com/#!/search/?q=%23foo">#foo</a> hoge');
+assert.equal(al.autolinkTwitter("http://mixi.jp @yappo #foo hoge", "_blank"), '<a href="http://mixi.jp" target="_blank">http://mixi.jp</a> <a href="http://twitter.com/yappo" target="_blank">@yappo</a> <a href="https://twitter.com/#!/search/?q=%23foo" target="_blank">#foo</a> hoge');
 assert.equal(al.autolink("'"), '&#39;');
 
