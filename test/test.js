d4dc428b5d41d445c86822644c4cc57908716fe1
OkuriSae <sae.okuri@gmail.com>
2020-02-13 19:10:19 +0900

    Support japanese (Hiragana/Katakana/Kanji)

--- test/test.js
+++ test/test.js
@@ -8,7 +8,10 @@ assert.equal(al.autolink("http://google.com/#yay"), '<a href="http://google.com/
 assert.equal(al.autolink("http://google.com/ yay!"), '<a href="http://google.com/">http://google.com/</a> yay!');
 assert.equal(al.autolink("http://google.com/ yay!", "_blank"), '<a href="http://google.com/" target="_blank">http://google.com/</a> yay!');
 assert.equal(al.autolink("foo > @yappo heh."), 'foo &gt; @yappo heh.');
-assert.equal(al.autolinkTwitter("http://mixi.jp @yappo #foo hoge"), '<a href="http://mixi.jp">http://mixi.jp</a> <a href="http://twitter.com/yappo">@yappo</a> <a href="https://twitter.com/#!/search/?q=%23foo">#foo</a> hoge');
-assert.equal(al.autolinkTwitter("http://mixi.jp @yappo #foo hoge", "_blank"), '<a href="http://mixi.jp" target="_blank">http://mixi.jp</a> <a href="http://twitter.com/yappo" target="_blank">@yappo</a> <a href="https://twitter.com/#!/search/?q=%23foo" target="_blank">#foo</a> hoge');
+assert.equal(al.autolinkTwitter("http://mixi.jp @yappo #foo hoge"), '<a href="http://mixi.jp">http://mixi.jp</a> <a href="http://twitter.com/yappo">@yappo</a> <a href="https://twitter.com/hashtag/foo">#foo</a> hoge');
+assert.equal(al.autolinkTwitter("http://mixi.jp @yappo #foo hoge", "_blank"), '<a href="http://mixi.jp" target="_blank">http://mixi.jp</a> <a href="http://twitter.com/yappo" target="_blank">@yappo</a> <a href="https://twitter.com/hashtag/foo" target="_blank">#foo</a> hoge');
+assert.equal(al.autolinkTwitter("#にほんご hoge"), '<a href="https://twitter.com/hashtag/%E3%81%AB%E3%81%BB%E3%82%93%E3%81%94">#にほんご</a> hoge');
+assert.equal(al.autolinkTwitter("#ニホンゴ hoge"), '<a href="https://twitter.com/hashtag/%E3%83%8B%E3%83%9B%E3%83%B3%E3%82%B4">#ニホンゴ</a> hoge');
+assert.equal(al.autolinkTwitter("#日本語 hoge"), '<a href="https://twitter.com/hashtag/%E6%97%A5%E6%9C%AC%E8%AA%9E">#日本語</a> hoge');
 assert.equal(al.autolink("'"), '&#39;');
 
